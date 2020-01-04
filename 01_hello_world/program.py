c8b9a89d560c3d900b13dd0e40f6ab4e1c6cdd0e
lordwayden <lordwayden@gmail.com>
2020-01-04 18:33:44 +0200

    first backup

---
+++ 01_hello_world/program.py
@@ -0,0 +1,9 @@
+print('------------------')
+print('         HELLO APP')
+print('------------------')
+print()
+
+user_text = input('What is your name? ')
+greeting = 'Greetings infidel ' + user_text
+
+print(greeting)
---
+++ 04_journal/program.py
@@ -0,0 +1,54 @@
+import journal
+
+
+# from journal import load, save
+# from journal import *
+
+
+def main():
+    print_header()
+    run_event_loop()
+
+
+def print_header():
+    print('-------------------------')
+    print('       JOURNAL APP')
+    print('-------------------------')
+
+
+def run_event_loop():
+    print('What do you want to do with your journal?')
+    cmd = None
+    journal_name = 'default'
+    journal_data = journal.load(journal_name)  # []  # list()
+
+    while cmd != 'x':
+        cmd = input('[L]ist entries, [A]dd an entry, E[x]it: ')
+        cmd = cmd.lower().strip()
+
+        if cmd == 'l':
+            list_entries(journal_data)
+        elif cmd == 'a':
+            add_entry(journal_data)
+        elif cmd != 'x':
+            print("Sorry, we don't understand '{}'.".format(cmd))
+
+    print('Done, goodbye.')
+    # savings into the folder
+    journal.save(journal_name, journal_data)
+
+
+def list_entries(data):
+    print('Your journal entries: ')
+    entries = reversed(data)
+    for idx, entry in enumerate(entries):
+        print('* [{}] {}'.format(idx + 1, entry))  # idx + 1 makes count from 1
+
+
+def add_entry(data):
+    text = input('Type your entry, <enter> to exit: ')
+    journal.add_entry(text, data)
+    # data.append(text)
+
+
+main()
